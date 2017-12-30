a3ca893f25f19e77797165c8ee5d5223c91450ff
Grzegorz Marchwinski <grzegorz.marchwinski@itaka.pl>
2017-12-30 22:01:10 +0100

    feat(amqp): testing error handling

--- microservices/node-consumer/consumer.js
+++ microservices/node-consumer/consumer.js
@@ -8,15 +8,15 @@ const messageBus = require('message-bus/create-durable-queue-channel');
 let startConsumer = async (doAck) => {
     let channel = await messageBus.getChannel();   
     let queueName = messageBus.getQueueName();
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queueName);
+    console.log("[*] Waiting for messages in %s. To exit press CTRL+C", queueName);
     
     return await channel.consume(queueName, (msg) => {
-        console.log(" [x] Received %s", msg.content.toString());
+        console.log("\t[x] Received %s", msg.content.toString());
         stats.meter('consume').mark();
         if (doAck) {
             setTimeout(() => {
                 channel.ack(msg);
-                console.log(" [x] Acked %s", msg.content.toString());
+                console.log("\t[x] Acked %s", msg.content.toString());
             }, 0);
         }
     }, {noAck: !doAck});
--- microservices/node-producer/producer.js
+++ microservices/node-producer/producer.js
@@ -3,25 +3,34 @@ console.log('Producer started');
 const amqp = require('amqplib');
 
 const messageBus = require('message-bus/create-durable-queue-channel');
+
 let counter = 0;
 
-let startProducer = async () => {
-    let channel = await messageBus.getChannel();   
-    let queueName = messageBus.getQueueName();
-    console.log(` [*] Producer will send messages to queue "${queueName}"`);
-    
-    setInterval(() => {
+let sendHelloWorldCommand = async (channel, queueName) => {
+    try {
         counter++;
         let obj = {
             value: 'Hello World!' + Math.round(Math.random()*1000),
             counter
         }
         let payload = JSON.stringify(obj);
-        channel.sendToQueue(queueName, new Buffer(payload), { persistent: true});
-            // .catch(err => console.error('sendToQueue error', err));
-        console.log(" [x] Sent", payload);
-    }, 1000);
+        let sendResult = channel.sendToQueue(queueName, new Buffer(payload), { persistent: true});
+        console.log("[+] Sent", payload, sendResult);
+    } catch (err) {
+        console.error('[-] Another way to catch error?', err);
+    }
+};
+
+let startProducer = async () => {
+    let channel = await messageBus.getChannel();   
+    channel.on('error', err => {
+        console.error('[-] producer channel error', err);
+    });
+    let queueName = messageBus.getQueueName();
+    console.log(`[+] Producer will send messages to queue "${queueName}"`);
+    
+    setInterval(() => sendHelloWorldCommand(channel, queueName), 1000);
 };
 
 startProducer()
-    .catch(err => console.error(err));
+    .catch(err => console.error('[-] start producer error', err));
