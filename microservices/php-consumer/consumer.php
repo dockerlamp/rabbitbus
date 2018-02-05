8ba09baea9790e4ba84de3d83efbc823f47d8150
Grzegorz Marchwinski <yankeepl@gmail.com>
2018-02-05 16:44:15 +0100

    feat(php-consumer):  multi commands handler

--- microservices/php-consumer/consumer.php
+++ microservices/php-consumer/consumer.php
@@ -1,5 +1,4 @@
 <?php
-$QUEUE_NAME = 'durable';
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
@@ -36,21 +35,67 @@ $connection = new \PhpAmqpLib\Connection\AMQPConnection(
 
 print "connected\n";
 $channel = $connection->channel();
-
-$channel->queue_declare($QUEUE_NAME, false, true, false, false);
-
-$callback = function($msg){
-  echo "\t[x] Received {$msg->body}\n";
-  $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
-  echo "\t[x] Acked\n";
-};
+register_shutdown_function(function() use ($channel, $connection) {
+    echo "closing rabbit connection\n";
+    $channel->close();
+    $connection->close();
+});
 
 $channel->basic_qos(null, 1, null);
-$channel->basic_consume($QUEUE_NAME, '', false, false, false, false, $callback);
 
-while(count($channel->callbacks)) {
-    $channel->wait();
+class Consumer {
+    private $QUEUE_PREFIX = 'durable';
+    private $channel;
+    function __construct($channel) {
+        $this->channel = $channel;
+    }
+
+    private function formatQueueName($command) {
+        return "{$this->QUEUE_PREFIX}.{$command}";
+    }
+
+    public function addCommandHandler($command, $handler) {
+        $callback = function($msg) use ($command, $handler) {
+            echo "\t[x] Received {$msg->body}\n";
+            try {
+                $handler($msg->body);
+                $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+                echo "\t[x] Acked\n";
+            } catch (\Exception $err) {
+                $msg->delivery_info['channel']->basic_nack($msg->delivery_info['delivery_tag']);
+                echo "\t[x] Nacked\n";
+            }
+        };
+        $this->initializeQueue($command);
+        $queueName = $this->formatQueueName($command);
+        $this->channel->basic_consume($queueName, '', false, false, false, false, $callback);
+        echo "command handler initialized {$command}\n";
+    
+    }
+
+    private function initializeQueue($command) {
+        $queueName = $this->formatQueueName($command);
+        $this->channel->queue_declare($queueName, false, true, false, false);
+        print "[*] Queue declared {$queueName}\n";
+    }    
+
+    public function consume() {
+        print "[*] waiting for commands\n";
+        while(count($this->channel->callbacks)) {
+            $this->channel->wait();
+        }
+    }
 }
 
-$channel->close();
-$connection->close();
+$HW2_CMD = 'hello.world.2';
+$HW3_CMD = 'hello.world.3';
+
+$consumer = new Consumer($channel);
+$consumer->addCommandHandler($HW2_CMD, function($body) {
+    echo "\t[x] received command HW2....\n";
+});
+$consumer->addCommandHandler($HW3_CMD, function($body) {
+    echo "\t[x] received command HW3....\n";
+});
+
+$consumer->consume();
